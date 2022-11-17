60e11ca445101e8d241a813bbb823ec7484da1a5
chosangwoo <kajer102@gmail.com>
2022-11-17 21:37:54 +0900

    Initial Commit

---
+++ InstaSearchView/ResultCell.swift
@@ -0,0 +1,22 @@
+//
+//  ResultCell.swift
+//  InstaSearchView
+//
+//  Created by 조상우 on 2022/10/31.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+    }
+    
+    func configure(_ imageName: String) {
+        thumbnailImageView.image = UIImage(named: imageName)
+    }
+}
